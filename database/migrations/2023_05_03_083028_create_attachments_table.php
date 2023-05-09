726268e34ab15ae0f939f9fb53af400743683a01
Brucenigel <106945745+Brucenigel@users.noreply.github.com>
2023-05-09 13:54:08 +0800

    Setup Migration (#2)
    Co-authored-by: Brucenigel <brucenige.vilo@sun-asterisk.com>

--- database/migrations/2023_05_03_083028_create_attachments_table.php
+++ database/migrations/2023_05_03_083028_create_attachments_table.php
@@ -10,14 +10,15 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('attachments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('address');
-            $table->integer('contact');
-            $table->date('birthdate');
+            $table->string('url');
+            $table->string('file_name');
+            $table->string('extension');
+            $table->string('type');
+            $table->morphs('attachable');
             $table->timestamps();
+
         });
     }
 
@@ -26,6 +27,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('attachments');
     }
 };
